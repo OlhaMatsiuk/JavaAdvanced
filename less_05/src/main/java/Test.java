2322abd46cec6432803545e2d761fa6aa0928580
Olha Zakharchuk <o.zakharchuk13@gmail.com>
2019-09-23 12:48:07 +0300

    one

--- less_05/src/main/java/Test.java
+++ less_05/src/main/java/Test.java
@@ -1,10 +1,6 @@
-import java.util.Date;
 
-import logos.domain.Magazine;
 import logos.domain.User;
-import logos.service.MagazineService;
 import logos.service.UserService;
-import logos.service.impl.MagazineServiceImpl;
 import logos.service.impl.UserServiceImpl;
 
 public class Test {
